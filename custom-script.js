69eb292cee6a9f20ece94843523bce5ad72f913e
Gabe Irina <irinagabe@outlook.com>
2019-12-09 09:50:30 +0300

    jQuery functions for toggling icons & descriptions

---
+++ custom-script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $("#design").click(function () {
+    $("#design-icon").slideToggle(1000);
+    $("#design-description").slideToggle(1000);
+  });
+  $("#dev").click(function () {
+    $("#dev-icon").slideToggle(1000);
+    $("#dev-description").slideToggle(1000);
+  });
+  $("#prod").click(function () {
+    $("#prod-icon").slideToggle(1000);
+    $("#prod-description").slideToggle(1000);
+  });
+
